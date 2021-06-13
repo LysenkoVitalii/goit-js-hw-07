244052a2fa66981ee6cef15bba088a8156413d98
Vitalii Lysenko <vetalyathebest2009@gmail.com>
2021-06-13 16:57:14 +0300

    ре-дедлайн

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,36 @@
-let inputVal = document.getElementById("validation-input");
+// let inputVal = document.getElementById("validation-input");
 
-let totalLenght = inputVal.getAttribute("data-length");
-let intTotallenght = parseInt(totalLenght, 10);
+// let totalLenght = inputVal.getAttribute("data-length");
+// let intTotallenght = parseInt(totalLenght, 10);
+
+// const validationInput = function() {
+//     if (inputVal.value.length == intTotallenght) {
+//         inputVal.classList.remove("invalid");
+//         inputVal.classList.add("valid");
+//     } else {
+//         inputVal.classList.remove("valid");
+//         inputVal.classList.add("invalid");
+//     }
+// };
+// inputVal.addEventListener('focusout', validationInput);
+
+
+let inputVal = document.getElementById("validation-input");
+let totalLenght = +inputVal.getAttribute("data-length");
+
 
 const validationInput = function() {
-    if (inputVal.value.length == intTotallenght) {
-        inputVal.classList.remove("invalid");
-        inputVal.classList.add("valid");
-    } else {
-        inputVal.classList.remove("valid");
-        inputVal.classList.add("invalid");
-    }
+if (inputVal.value.length === totalLenght) {
+updateClass ("valid","invalid")
+} else {
+updateClass ("invalid", "valid")
+}
 };
+
+function updateClass (addClass, remClass) {
+inputVal.classList.remove(remClass);
+inputVal.classList.add(addClass);
+}
+
+
 inputVal.addEventListener('focusout', validationInput);
